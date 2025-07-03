3631b894e644f8691fb70e38e1c16cc98f6e8123
hj.hwang <hj.hwang@infoin.com>
2025-07-03 22:29:27 +0900

    디자인 수정

--- js/port.js
+++ js/port.js
@@ -8,14 +8,21 @@ menuBtn.addEventListener("click", () => {
 
 const gridItems = document.querySelectorAll(".i-grid");
 const showImg = document.querySelector(".show-img img");
+const imgContainer = document.querySelector(".show-img .img-container");
 
 gridItems.forEach((item) => {
   item.addEventListener("mouseenter", () => {
     const img = item.querySelector("img");
-    const newSrc = img.getAttribute("src");
+    const newSrc = img.getAttribute("data-full") || img.getAttribute("src");
     const newAlt = img.getAttribute("alt") || "";
+
+    // animation 재시작을 위해 class 재적용
+    imgContainer.classList.remove("active");
+    void imgContainer.offsetWidth; // 리플로우
 
     showImg.setAttribute("src", newSrc);
     showImg.setAttribute("alt", newAlt);
+
+    imgContainer.classList.add("active");
   });
 });
